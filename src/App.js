27e84585161c8ade42f6ea3c874d7ed264ff0bb8
o0dante0o <j.a@a-vision.co>
2024-04-30 15:37:57 -0600

    30-04-2024 | Topbar and theme completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {ColorModeContext, useMode} from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes,Route } from 'react-router-dom';
+
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+// import Team from './scenes/team';
+// import Invoices from './scenes/invoices';
+// import Contacts from './scenes/contacts';
+// import Bar from './scenes/bar';
+// import Form from './scenes/form';
+// import line from './scenes/line';
+// import Pie from './scenes/pie';
+// import FAQ from './scenes/faq';
+// import Geography from './scenes/geography';
+// import Calendar from './scenes/calendar'
+
+
+
+
+
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className='content'>
+            <Topbar/>
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              {/* <Route path='/team' element={<Dashboard />} />
+              <Route path='/cibtacts' element={<Dashboard />} />
+              <Route path='/invoices' element={<Dashboard />} />
+              <Route path='/form' element={<Dashboard />} />
+              <Route path='/bar' element={<Dashboard />} />
+              <Route path='/pie' element={<Dashboard />} />
+              <Route path='/line' element={<Dashboard />} />
+              <Route path='/faq' element={<Dashboard />} />
+              <Route path='/geography' element={<Dashboard />} />
+              <Route path='/calendar' element={<Dashboard />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
   );
 }
 
