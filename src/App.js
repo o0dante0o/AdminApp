c23838ef8fd505cb74974814ff8d659c66e8f213
o0dante0o <j.a@a-vision.co>
2024-05-02 16:55:22 -0600

    02-05-2024 | invoices feature completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
-// import Invoices from './scenes/invoices';
+import Invoices from './scenes/invoices';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import line from './scenes/line';
@@ -35,10 +35,10 @@ function App() {
             <Topbar/>
             <Routes>
               <Route path='/' element={<Dashboard />} />
-               <Route path='/team' element={<Team />} />
+              <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
-              {/*<Route path='/invoices' element={<Invoices />} />
-              <Route path='/form' element={<Form />} />
+              <Route path='/invoices' element={<Invoices />} />
+              {/*<Route path='/form' element={<Form />} />
               <Route path='/bar' element={<Bar />} />
               <Route path='/pie' element={<Pie />} />
               <Route path='/line' element={<Line />} />
